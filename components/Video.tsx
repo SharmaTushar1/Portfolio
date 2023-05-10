5e91580b408c6521a85963a2bd4f922291262bdc
SharmaTushar1 <tusharsharma2440160@gmail.com>
2023-05-10 20:55:30 +0530

    Fix video bg in small devices

--- components/Video.tsx
+++ components/Video.tsx
@@ -11,7 +11,7 @@ const Video = ({url}: Props) => {
         loop
         autoPlay
         muted
-        className='fixed right-0 bottom-0 min-w-full min-h-full -z-[2]'
+        className='fixed right-0 bottom-0 min-w-full min-h-full -z-[2] object-cover'
       >
         <source src={url} type='video/mp4' />
       </video>

15facc18f9bcf44126f3d9dedb54079dbadfa825
SharmaTushar1 <tusharsharma2440160@gmail.com>
2023-05-10 17:24:01 +0530

    Add socials and resume page

---
+++ pages/twitter.tsx
@@ -0,0 +1,27 @@
+import Error from '@/components/Error';
+import { useRouter } from 'next/router'
+import React, { useEffect, useState } from 'react'
+
+const resume = () => {
+  const router = useRouter();
+  const [error, setError]: any = useState(null)
+
+  useEffect(() => {
+    async function redirect() {
+      try { 
+        await router.push("https:www.twitter.com/SharmaTushar1_");
+      } catch (err) {
+        setError(err);
+      }
+    }
+    redirect();
+  }, []);
+
+  if (error) {
+    return <Error />;
+  }
+
+  return null;
+}
+
+export default resume

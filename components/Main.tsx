40ac0de8f067c10a53286cafc1742b366fb8aa9c
SharmaTushar1 <tusharsharma2440160@gmail.com>
2023-05-10 17:24:32 +0530

    Add favicon and title

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import Video from './Video';
+import Head from 'next/head';
 
 const Main = () => {
   return (
     <div>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <title>Tushar Sharma</title>
+      </Head>
       <Video url="/aerial1.mp4" />
     </div>
   )

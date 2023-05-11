131cf4ffbd46555986ef7a429958928ad79635cb
SharmaTushar1 <tusharsharma2440160@gmail.com>
2023-05-11 20:31:13 +0530

    Add loading animation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react';
+import '../styles/Main.css'
+import '../styles/Navbar.css'
+
+
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const loadingElement = document.getElementById('loading');
+    if (loadingElement) {
+      loadingElement.classList.add('loaded');
+    }
+  }, []);
   return <Component {...pageProps} />
 }

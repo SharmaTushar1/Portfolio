9b56035a1cf524cdfe9b7501d8d9877f1d728660
SharmaTushar1 <tusharsharma2440160@gmail.com>
2023-08-18 11:12:14 +0530

    UI Fix for mobile screens blog pages

--- src/app/blog/components/Post.tsx
+++ src/app/blog/components/Post.tsx
@@ -17,11 +17,13 @@ export default function Post({post}: Props) {
       </h2>
       <p>{ description }</p>
       <br />
-      <div className='flex justify-between'>
-        <div className='flex w-max-[80%]'>
-          {tags.map( tag => (
-            <div className='border rounded-full px-2 mr-2 text-xs py-1 bg-zinc-200 dark:bg-zinc-800' key={tag}>{tag}</div>
-          ))}
+      <div className='flex flex-col sm:flex-row justify-between'>
+        <div className='flex flex-col sm:flex-row w-max-[80%]'>
+          <div className='flex flex-wrap -mt-2 sm:mt-0'>
+            {tags.map( tag => (
+              <div className='border rounded-full px-2 mr-2 text-xs py-1 bg-zinc-200 dark:bg-zinc-800 w-fit mt-2 sm:mt-0' key={tag}>{tag}</div>
+            ))}
+          </div>
           {/* TODO: for this tags also I'll have to have a different page. So, all the posts with tag Algorithms in one page, same for say front-end. */}
           <div>{ timeToRead } min read</div>
         </div>

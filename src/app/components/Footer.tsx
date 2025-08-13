2a2914e0639937651312c20a20af95d9fe0b3c44
Tushar Sharma <62995185+SharmaTushar1@users.noreply.github.com>
2025-08-14 00:17:59 +0530

    update footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
         <Link href="https://www.linkedin.com/in/tusharsharma31" target="_blank">
           <Image className="px-2" width={48} height={48} src="https://img.icons8.com/color/48/linkedin.png" alt="LinkedIn"/>
         </Link>
-        <Link href="https://www.youtube.com/" target="_blank">
+        <Link href="https://www.youtube.com/SharmaTushar1-yt" target="_blank">
           <Image className="px-2" width={48} height={48} src={'https://img.icons8.com/color/48/youtube-play.png'} alt="YouTube" />
         </Link>
         <Link href="https://www.github.com/SharmaTushar1" target="_blank">
@@ -17,7 +17,7 @@ export default function Footer() {
         <Link href="mailto:sharmatushar0k@gmail.com" target="_blank">
           <Image className="px-2" width={48} height={48} src={'https://img.icons8.com/color/48/gmail-new.png'} alt="Gmail" />
         </Link>
-        <Link href="https://www.twitter.com/SharmaTusharOne" target="_blank">
+        <Link href="https://www.twitter.com/SharmaTushar1" target="_blank">
           <Image className="px-2" width={48} height={48} src="https://img.icons8.com/fluency/48/twitterx.png" alt="Twitter"/>
         </Link>
         <Link href="https://buymeacoffee.com/SharmaTusharOne" target="_blank">
